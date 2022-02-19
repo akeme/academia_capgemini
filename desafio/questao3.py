5c8978811ca12c79a9975e2b855cbeeb42658bda
Mariana Ogawa <mari.ogawa@gmail.com>
2022-02-19 17:08:54 -0300

    3 questao

--- desafio/questao3.py
+++ desafio/questao3.py
@@ -5,18 +5,32 @@ Dada uma string qualquer, desenvolva um algoritmo que encontre o número de pare
 são anagramas.
 '''
 
+from operator import sub
+
+
 def anagrama(palavra):
-    quantidade = 0
-    aux = []
-    return 0
+    qtd = 0
+    lista_substrings = substrings(palavra)
+    for i in range(0,len(lista_substrings)):
+        for j in range(1,len(lista_substrings)):
+            print(i,lista_substrings[i],j, lista_substrings[j])
+            if (i!=j):
+                if lista_substrings[i] == lista_substrings[j] [::-1]:
+                    print(i,lista_substrings[i],j, lista_substrings[j] [::-1])
+                    qtd +=1
+    return qtd
+    
 
 
 
 def substrings(palavra):
     aux = []
-    for i in range(0, len(palavra)):
-        for j in range(0,len(palavra)):
-            print(palavra[i:i+1])
-    return True
+    for i in range(0, len(palavra)+1):
+        for j in range(len(palavra),0,-1):
+            if (i!=j) and (i < j):
+                aux.append(palavra[i:j])
+    return aux
 
-anagrama("ovo")
+
+
+print(anagrama("ifailuhkqq"))
